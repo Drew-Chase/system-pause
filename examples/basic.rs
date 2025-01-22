5aa624fa4cde997e2489f5520e7b8dbbc5a2b331
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-22 11:23:01 -0500

    Add pause to basic example for user interaction
    This update includes a pause in the basic example to halt program execution and prompt the user before continuing. It enhances the demonstration by making it more interactive and clear.

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,7 +1,9 @@
-use system_pause::pause;
+use system_pause::{pause};
 
 fn main() {
-	println!("Hello, world!");
+    println!("Hello, world!");
+    // Pauses the program execution and prompts the user with a default message.
+    // The program will wait until the user presses the Enter key.
     pause!();
-	println!("Goodbye, world!");
+    println!("Goodbye, world!");
 }
